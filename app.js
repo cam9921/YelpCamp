682517c7cf520136c4a56e143a3dedf098d3cb9c
Camden Taylor <camdentaylor1992@gmail.com>
2019-01-12 20:00:52 -0700

    Created campgrounds database. Initialized with some data. Updated code to pull campgrounds from database and allow user to add campgrounds to database.

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      app = express();
+
+mongoose.connect('mongodb://localhost/yelp_camp');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-let campgrounds = [
-    {
-        name: 'Gergle Gargle',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSem9E-gtolyMnCAWd8xmE5M_b-ynz4b_0CyIm5FAmWUtM57U1b'
-    }, 
-    {
-        name: 'Dingle Dangle',
-        image: 'http://www.freakingnews.com/pictures/82000/Redneck-Kids-Camping--82215.jpg'
-    }, 
-    {
-        name: 'Boofaloof',
-        image: 'https://i.pinimg.com/originals/db/7e/af/db7eafab1d65936fce7f5cafa1199d80.png'
-    }
-];
+//Set up our Schema
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    imageURL: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {
-        campgrounds: campgrounds
-    });
+    //Get all campgrounds
+    Campground.find({}, (err, campgrounds) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {
+                campgrounds: campgrounds
+            });
+        }
+    })
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -36,16 +39,22 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', (req, res) => {
-    //Get data from a form and add it to the campgrounds array.
     const name = req.body.name;
-    const image = req.body.image;
-    let newCampground = {
-        name: name,
-        image: image
+    const imageURL = req.body.imageURL;
+    const newCampground = {
+        name: name, 
+        imageURL: imageURL
     };
-    campgrounds.push(newCampground);
-    //Redirect to the campgrounds page.
-    res.redirect('/campgrounds');
+
+    Campground.create(newCampground, (err, campground) => {
+            if(err) {
+                console.log(err)
+            } else {
+                console.log(`Created Campground ${campground.name}`)
+                res.redirect('/campgrounds');
+            };
+        }
+    );
 });
 
 app.listen(3000, () => {
