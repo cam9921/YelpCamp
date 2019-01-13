455dae564ff25d465a7947eb542585ee66a98260
Camden Taylor <camdentaylor1992@gmail.com>
2019-01-12 20:57:40 -0700

    Added description property to new campground form, and to DB model

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.set('view engine', 'ejs');
 //Set up our Schema
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    imageURL: String
+    imageURL: String, 
+    description: String
 });
 
 const Campground = mongoose.model('Campground', campgroundSchema);
@@ -21,29 +22,47 @@ app.get('/', (req, res) => {
     res.render('landing');
 });
 
+//INDEX route - show all campgrounds
 app.get('/campgrounds', (req, res) => {
     //Get all campgrounds
     Campground.find({}, (err, campgrounds) => {
         if(err) {
             console.log(err);
         } else {
-            res.render('campgrounds', {
+            res.render('index', {
                 campgrounds: campgrounds
             });
         }
     })
 });
 
+//NEW - show form to create new campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
 });
 
+//SHOW route - show information on one particular campground.
+app.get('/campgrounds/:id', (req, res) => {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('show', {
+                campground: foundCampground
+            });
+        }
+    });
+});
+
+//CREATE route - add new campground to database
 app.post('/campgrounds', (req, res) => {
     const name = req.body.name;
     const imageURL = req.body.imageURL;
+    const description = req.body.description;
     const newCampground = {
         name: name, 
-        imageURL: imageURL
+        imageURL: imageURL,
+        description: description
     };
 
     Campground.create(newCampground, (err, campground) => {
